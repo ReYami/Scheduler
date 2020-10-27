2adfabd0ae51e38ac5688e5fa2d6e6a58e316f8a
ReYami <73469611+ReYami@users.noreply.github.com>
2020-10-27 11:30:40 +0400

    Commit from mac Scheduler class

---
+++ src/Event.java
@@ -0,0 +1,31 @@
+import java.io.PrintStream;
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class Event {
+    private String name;
+    private String organization;
+    private int priorityLv;
+    private String details;
+
+    public Event(){
+        Scanner input = new Scanner(System.in);
+        out.println("What's this task called?");
+        name = input.nextLine();
+        out.println("Who is this task for?");
+        organization = input.nextLine();
+        out.println("On a scale of 1 to ten, how important is this task?");
+        priorityLv = input.nextInt();
+        input.nextLine();
+        out.println("Are there any other details I should record for you?");
+        input.nextLine();
+    }
+
+    public String toString(){
+            return "Task name: " + name +
+                    "\n Organization: " + organization +
+                    "\n Priority level: " + priorityLv +
+                    "\n Details: " + details;
+
+    }
+}
---
+++ src/Scheduler.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.util.Date;
+
+public class Scheduler {
+    public static void main(String [] args){
+        boolean active;
+
+
+    }
+}
