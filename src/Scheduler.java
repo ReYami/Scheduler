1122ad639c848511132faa56e523f7ca629eb60e
ReYami <73469611+ReYami@users.noreply.github.com>
2020-10-27 12:54:17 +0400

    Commit from mac Scheduler class Event class writer class

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,11 +1,14 @@
 import java.io.PrintStream;
 import static java.lang.System.out;
+
 import java.util.Scanner;
 import java.util.Date;
 
 public class Scheduler {
     public static void main(String [] args){
         boolean active;
+        out.println("Hey, I'll schedule some events for you");
+        //its really more of a task list right now but whatever.
 
 
     }
---
+++ src/writer.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.io.PrintStream;
+import static java.lang.System.out;
+
+public class writer {
+    Date today;
+    String pattern;
+    SimpleDateFormat Pattern;
+
+
+    public writer() throws FileNotFoundException{
+        //formatting the date
+        today = new Date();
+        pattern = "yyyy/MM/dd";
+        Pattern = new SimpleDateFormat(pattern);
+        Pattern.format(today);
+        //creating a file to read the date from.
+        PrintStream x = new PrintStream(new File(today + " TASKS.txt"));
+        PrintStream console = System.out;
+
+    }
+
+    public void setOutputConsole(PrintStream console){
+        System.setOut(console);
+    }
+    public void setOutputTasklist(PrintStream tasklist){
+        System.setOut(tasklist);
+    }
+}
